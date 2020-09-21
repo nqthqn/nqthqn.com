968ed6af5cdb7183a3904514d6abbc2f5f4b133f
Nathan Nichols <nnichols@concentricsky.com>
2020-09-21 08:00:42 -0700

    Improve transcript formatting, ep 6

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 hugo new --kind episodes content/episodes/$1 &&
-cat ~/Downloads/$1.txt >> content/episodes/$1/index.md &&
+cat ~/Downloads/$1.txt | tr '\n' ' ' >> content/episodes/$1/index.md &&
 gsutil cp ~/Downloads/$1.m4a gs://nqthqn-dat
